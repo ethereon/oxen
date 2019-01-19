431b8854ee8d2f2f62b3dd3df468821da2251ade
Saumitro Dasgupta <sd@cs.stanford.edu>
2019-01-18 16:51:28 -0800

    Missing subscriber support

--- oxen/event.py
+++ oxen/event.py
@@ -9,11 +9,18 @@ class EventEmitter:
     def subscribe(self, subscriber):
         self.subscribers.add(subscriber)
 
-    def unsubscribe(self, subscriber):
-        self.subscribers.remove(subscriber)
+    def unsubscribe(self, subscriber, ignore_missing=False):
+        try:
+            self.subscribers.remove(subscriber)
+        except KeyError:
+            if not ignore_missing:
+                raise
 
     def publish(self, event):
         # Clone the current subscribers since a subscriber
         # might mutate the subscription in response to a published event.
         for subscriber in set(self.subscribers):
             subscriber(event)
+
+    def is_subscriber(self, handler):
+        return handler in self.subscribers
