9b780158563142fffba0ca6e6b6d12e858cc0f57
Saumitro Dasgupta <sd@cs.stanford.edu>
2026-07-30 16:45:12 -0400

    Blank slate

--- oxen/__init__.py
+++
@@ -1,6 +0,0 @@
-from .path import Path
-from .process import Process
-from .rsync import auto_rsync
-from .session import Session
-from .task import Lazy
-from .watch import Watch
--- oxen/client/src/app.ts
+++
@@ -1,6 +0,0 @@
-import * as ReactDOM from 'react-dom';
-import * as React from 'react';
-
-import { AppView } from './app-view';
-
-ReactDOM.render(React.createElement(AppView), document.getElementById('app'));
